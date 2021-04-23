05528e7c16376a25ffff8562670a75f9f5ec0ab0
hamzsait <hamzasait@utexas.edu>
2021-04-22 22:10:50 -0500

    finalized construction of CRUDE functions

--- server.js
+++ server.js
@@ -4,8 +4,36 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-function add(item){
-    
+function addJSON(item){
+    output = JSON.parse(fs.readFileSync('./db/db.json'))
+    output.push(item)
+    index = 0
+    for (o of output){
+        o.id = index
+        index += 1
+    }
+    output = JSON.stringify(output)
+    fs.writeFile('./db/db.json',output,(err) => console.log(err))
+    return
+}
+
+function deleteJSON(id,res){
+    output = JSON.parse(fs.readFileSync('./db/db.json'))
+        res.send('Deleted')
+        for (x = 0; x<output.length;x++){
+            if (output[x].id == id){
+                output.splice(id,1)
+            }
+        }
+        index = 0
+        for (o of output){
+            o.id = index
+            index += 1
+        }
+    console.log(output)
+    output = JSON.stringify(output)
+    fs.writeFile('./db/db.json',output,(err) => console.log(err))
+    return
 }
 
 app.use(express.json());
@@ -14,8 +42,9 @@ app.use(express.static('public'));
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, './public/index.html')))
 app.get("/notes", (req,res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 app.get("/api/notes", (req,res) => res.json(JSON.parse(fs.readFileSync('./db/db.json'))))
-app.post("/api/notes", (req,res) => )
-
+app.post("/api/notes", (req,res) => addJSON(req.body))
+app.delete('/api/notes/:id', (req, res) => deleteJSON(req.params.id,res))
 
 app.listen(PORT);
+
 
